93e26ac8e7e84318ea0f15544ea9849789cc18c0
hello2727 <jihye7369@gmail.com>
2020-12-18 01:21:36 +0900

    글메모 데이터베이스 인스턴스 가져오기

--- TodoMemo/app/src/main/java/org/jh/todomemo/CreateWritingMemo.java
+++ TodoMemo/app/src/main/java/org/jh/todomemo/CreateWritingMemo.java
@@ -1,9 +1,12 @@
 package org.jh.todomemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.widget.EditText;
+
+import org.jh.todomemo.db.database.writingMemoDatabase;
 
 public class CreateWritingMemo extends AppCompatActivity {
     EditText cwm_previewWTitle, cwm_previewWContents;
@@ -15,6 +18,13 @@ public class CreateWritingMemo extends AppCompatActivity {
 
         cwm_previewWTitle = findViewById(R.id.cwm_insertTitle);
         cwm_previewWContents = findViewById(R.id.cwm_insertContents);
+
+        /* 글메모 데이터베이스 인스턴스 가져오기 */
+        writingMemoDatabase db = Room.databaseBuilder(getApplicationContext(),
+                writingMemoDatabase.class, "writingMemoDB").build();
+    }
+
+    public void saveDB(){
 
     }
 }
