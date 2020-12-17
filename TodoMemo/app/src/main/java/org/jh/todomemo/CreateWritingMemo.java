53a2f8f0aa3c00c3defbb9635974158f4eaa130a
hello2727 <jihye7369@gmail.com>
2020-12-18 01:29:53 +0900

    글메모 저장 함수 생성

--- TodoMemo/app/src/main/java/org/jh/todomemo/CreateWritingMemo.java
+++ TodoMemo/app/src/main/java/org/jh/todomemo/CreateWritingMemo.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.widget.EditText;
 
 import org.jh.todomemo.db.database.writingMemoDatabase;
@@ -22,9 +23,12 @@ public class CreateWritingMemo extends AppCompatActivity {
         /* 글메모 데이터베이스 인스턴스 가져오기 */
         writingMemoDatabase db = Room.databaseBuilder(getApplicationContext(),
                 writingMemoDatabase.class, "writingMemoDB").build();
+
+
     }
 
-    public void saveDB(){
-
+    /* 글메모 DB에 메모 저장 */
+    public void saveDB(writingMemoDatabase db, Editable title, Editable content){
+        db.writingMemoDao().insertWritingMemos();
     }
 }
