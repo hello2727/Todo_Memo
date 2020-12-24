b8a394f61e54fda9ba8daca32422933f7e7b2c8a
hello2727 <jihye7369@gmail.com>
2020-12-25 02:24:10 +0900

    글메모 roomdb dao에 구문 추ㅜ가

--- TodoMemo/app/src/main/java/org/jh/todomemo/db/dao/writingMemoDao.java
+++ TodoMemo/app/src/main/java/org/jh/todomemo/db/dao/writingMemoDao.java
@@ -22,9 +22,19 @@ public interface writingMemoDao {
     @Delete
     void deleteWritingMemos(writingMemo... writingMemos);
 
+    //delete all query
+    @Delete
+    void reset(List<writingMemo> writingMemos);
+
+    //update query
+    @Query("UPDATE writingMemo_table SET writing_Title = :title AND writing_content = :content WHERE ID = :order")
+    void updateWritingMemo(int order, String title, String content);
+
+    //get all data query
     @Query("SELECT * FROM writingMemo_table")
     List<writingMemo> getAllMemo();
 
+    //load query
     @Query("SELECT * FROM writingMemo_table WHERE writing_Title = :wTitle AND writing_content = :content")
     writingMemo[] loadWritingMemo(String wTitle, String content);
 }
