26854e73aaf01009e023d9c25b29f502db9991b3
hello2727 <jihye7369@gmail.com>
2020-12-29 13:06:29 +0900

    글메모 viewmodel 추가

---
+++ TodoMemo/app/src/main/java/org/jh/todomemo/ViewModel/writingMemoViewModel.java
@@ -0,0 +1,34 @@
+package org.jh.todomemo.ViewModel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import org.jh.todomemo.db.entity.writingMemo;
+import org.jh.todomemo.db.entity.writingMemoRepository;
+
+import java.util.List;
+
+/* UI에 데이터를 제공하고 변경된 사항들을 업데이트 */
+public class writingMemoViewModel extends AndroidViewModel {
+    //Repository 클래스에서 필요한 부분을 ViewModel에 연결.
+    //- word리스트 불러오기, word insert하기 부분
+    private writingMemoRepository mwritingMemoRepository;
+
+    private LiveData<List<writingMemo>> mAllWritingMemos;
+
+    public writingMemoViewModel(Application application) {
+        super(application);
+        mwritingMemoRepository = new writingMemoRepository(application);
+        mAllWritingMemos = mwritingMemoRepository.getAllWritingMemos();
+    }
+
+    LiveData<List<writingMemo>> getAllWritingMemos() {
+        return mAllWritingMemos;
+    }
+
+    public void insert(writingMemo writingMemo) {
+        mwritingMemoRepository.insert(writingMemo);
+    }
+}
