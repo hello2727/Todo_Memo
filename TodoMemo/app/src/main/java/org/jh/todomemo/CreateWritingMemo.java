a0f0e98d00164d2705d923401de05f1ecfe20211
hello2727 <jihye7369@gmail.com>
2020-12-31 01:59:39 +0900

    글메모 정보전달

--- TodoMemo/app/src/main/java/org/jh/todomemo/CreateWritingMemo.java
+++ TodoMemo/app/src/main/java/org/jh/todomemo/CreateWritingMemo.java
@@ -3,13 +3,17 @@ package org.jh.todomemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.widget.EditText;
 
 import org.jh.todomemo.db.database.writingMemoDatabase;
 
 public class CreateWritingMemo extends AppCompatActivity {
+    public static final String EXTRA_REPLY1 = "wTitle";
+    public static final String EXTRA_REPLY2 = "wContent";
     EditText cwm_previewWTitle, cwm_previewWContents;
 
     @Override
@@ -20,11 +24,24 @@ public class CreateWritingMemo extends AppCompatActivity {
         cwm_previewWTitle = findViewById(R.id.cwm_insertTitle);
         cwm_previewWContents = findViewById(R.id.cwm_insertContents);
 
-        /* 글메모 데이터베이스 인스턴스 가져오기 */
-        writingMemoDatabase db = Room.databaseBuilder(getApplicationContext(),
-                writingMemoDatabase.class, "writingMemoDB").build();
+//        /* 글메모 데이터베이스 인스턴스 가져오기 */
+//        writingMemoDatabase db = Room.databaseBuilder(getApplicationContext(),
+//                writingMemoDatabase.class, "writingMemoDB").build();
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        Intent replyIntent = new Intent(CreateWritingMemo.this, MWriting.class);
+        if(TextUtils.isEmpty(cwm_previewWTitle.getText()) && TextUtils.isEmpty(cwm_previewWContents.getText())) {
+            setResult(RESULT_CANCELED, replyIntent);
+        }else{
+            String wTitle = cwm_previewWTitle.getText().toString();
+            String wContent = cwm_previewWContents.getText().toString();
+            replyIntent.putExtra(EXTRA_REPLY1, wTitle);
+            replyIntent.putExtra(EXTRA_REPLY2, wContent);
+            setResult(RESULT_OK, replyIntent);
+        }
+        finish();
     }
 
     /* 글메모 DB에 메모 저장 */
