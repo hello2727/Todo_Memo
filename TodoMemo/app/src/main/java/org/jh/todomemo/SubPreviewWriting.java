810c848b2b437cb0c5a819bf5664d2381f9a1ce3
hello2727 <jihye7369@gmail.com>
2021-01-06 16:11:11 +0900

    글메모 상세보기 액티비티의 제목 부분을 터치하면 에디트텍스트가 나와 글메모가 수정이 가능하도록 만들고 포커스, 키보드 올라오게 하기 + 앱을 벗어나면 키보드 사라지게 하기+UI구성 변화

--- TodoMemo/app/src/main/java/org/jh/todomemo/SubPreviewWriting.java
+++ TodoMemo/app/src/main/java/org/jh/todomemo/SubPreviewWriting.java
@@ -2,14 +2,20 @@ package org.jh.todomemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.jh.todomemo.R;
 
 public class SubPreviewWriting extends AppCompatActivity {
     TextView previewWTitle, previewWContents;
+    EditText et_previewWTitle, et_previewWContents;
+    View sbw_line;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,45 @@ public class SubPreviewWriting extends AppCompatActivity {
 
         previewWTitle = findViewById(R.id.previewWTitle);
         previewWContents = findViewById(R.id.previewWContents);
+        et_previewWTitle = findViewById(R.id.et_previewWTitle);
+        et_previewWContents = findViewById(R.id.et_previewWContents);
+        sbw_line = findViewById(R.id.sbw_line);
 
+        //메모 상세내용 표시하기
         Intent intent = getIntent();
-        String wtitle = intent.getExtras().getString("wtitle");
+        final String wtitle = intent.getExtras().getString("wtitle");
         String wcontents = intent.getExtras().getString("wcontents");
-
         previewWTitle.setText(wtitle);
         previewWContents.setText(wcontents);
+
+        //제목을 클릭할 경우 메모의 제목 수정하기 기능이 나타난다.
+        previewWTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previewWTitle.setVisibility(View.GONE);
+                sbw_line.setVisibility(View.GONE);
+
+                et_previewWTitle.setVisibility(View.VISIBLE); //화면에 나타내고
+                et_previewWTitle.setText(wtitle); //값 설정하고
+                et_previewWTitle.requestFocus(); //포커스 주고
+                //키보드 올라오게 하기
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+            }
+        });
+        previewWContents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //키보드 사라지게 하기
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
 }
