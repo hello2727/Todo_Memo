5bef239e99ede7eab38213f5bb8042f958d2d473
hello2727 <jihye7369@gmail.com>
2021-01-23 12:07:52 +0900

    찍은 사진위에 낙서 기능 추가시도3

--- TodoMemo/app/src/main/java/org/jh/todomemo/View/PencilView.java
+++ TodoMemo/app/src/main/java/org/jh/todomemo/View/PencilView.java
@@ -1,32 +1,87 @@
 package org.jh.todomemo.View;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
+class Point{
+    float x;
+    float y;
+    int pValue; //손가락터치 상태
+    public Point(float x, float y, int value){
+        this.x = x;
+        this.y = y;
+        pValue = value;
+    }
+}
+class Scale{
+    int start, end, color; //DrawImage의 범위 및 색상
+    public Scale(int start, int end, int color){
+        this.start = start;
+        this.end = end;
+        this.color = color;
+    }
+}
+
 public class PencilView extends View {
-    Paint paint = new Paint();
-    Path path = new Path(); //자취를 저장할 객체
+    Paint paint;
+    ArrayList<Point> arrP; //Point 클래스 저장.
+    ArrayList<Scale> arrS; //Scale 클래스 저장.
+    final int WAIT = -1;
+    final int START = 0; //터치상태(down)
+    final int MOVE = 1; //터치상태(move)
+    int value, color, reStart, reEnd; //터치상태 저장변수, drawimage 색상, drawimage의 시작값 대입변수, drawimage의 끝값 변수
+
+    Bitmap bit;
 
     public PencilView(Context context) {
         super(context);
 
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(7f);
-        paint.setColor(Color.BLACK);
+        paint = new Paint();
+        arrP = new ArrayList<>();
+        arrS = new ArrayList<>();
+        color = Color.BLACK;
+        bit = null;
+//        paint.setColor(Color.BLACK);
     }
 
-    public void setColor(int color){
-        paint.setColor(color);
+    public void setColor(int pickedColor){
+        color = pickedColor;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawPath(path, paint);
+        canvas.drawColor(Color.TRANSPARENT);
+        reEnd = arrP.size();
+        arrS.add(new Scale(reStart, reEnd, color));
+
+        if(bit != null){
+            canvas.drawBitmap(bit, null, new Rect(0, 0, 600, 600), null);
+        }
+
+        for(int j = 0; j < arrS.size(); j++){
+            paint.setColor(arrS.get(j).color);
+            paint.setStrokeWidth(3f);
+
+            //arrS에 저장된 각 grawImage의 범위만큼만 loop 돌면서 image 그림.
+            for(int i = arrS.get(j).start; i < arrS.get(j).end; i++){
+                if(arrP.get(i).pValue == MOVE){
+                    canvas.drawLine(arrP.get(i-1).x, arrP.get(i-1).y, arrP.get(i).x, arrP.get(i).y, paint);
+                }
+
+                if(i == arrP.size() - 1){ //마지막에 그려진 drawimage의 끝 값이
+                    reStart = arrP.size(); //다음 drawimage의 처음 값으로 저장.
+                }
+            }
+        }
     }
 
     @Override
@@ -36,16 +91,17 @@ public class PencilView extends View {
 
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                path.moveTo(x, y);
+                value = START;
+                arrP.add(new Point(x, y, value));
                 break;
             case MotionEvent.ACTION_MOVE:
-                path.lineTo(x, y);
+                value = MOVE;
+                arrP.add(new Point(x, y, value));
+                invalidate();
                 break;
             case MotionEvent.ACTION_UP:
                 break;
         }
-
-        invalidate();
-        return true;
+        return false;
     }
 }
