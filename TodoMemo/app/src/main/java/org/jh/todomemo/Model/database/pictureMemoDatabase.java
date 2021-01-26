f5b1d4d8221eb8c605a55edb66c6c3f450b2f971
hello2727 <jihye7369@gmail.com>
2021-01-26 13:31:25 +0900

    사진메모 database 구현

---
+++ TodoMemo/app/src/main/java/org/jh/todomemo/Model/database/pictureMemoDatabase.java
@@ -0,0 +1,33 @@
+package org.jh.todomemo.Model.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import org.jh.todomemo.Model.dao.pictureMemoDao;
+import org.jh.todomemo.Model.entity.pictureMemo;
+
+@Database(entities = {pictureMemo.class}, version = 1, exportSchema = false)
+public abstract class pictureMemoDatabase extends RoomDatabase {
+    public abstract pictureMemoDao pictureMemoDao();
+
+    //singleton pattern, room database는 한개만 존재
+    private static pictureMemoDatabase INSTANCE;
+
+    public static pictureMemoDatabase getDatabase(final Context context) {
+        if(INSTANCE == null){
+            //동시에 2개의 INSTANCE가 생성되는 것을 막기위한 synchronized
+            synchronized (pictureMemoDatabase.class){
+                if(INSTANCE == null){
+                    //데이터베이스 생성부분
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            pictureMemoDatabase.class, "pictureMemo_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
